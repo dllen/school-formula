e0a5af2a2497092d61745cb649e00f2533dd0115
dllen <dllen@users.noreply.github.com>
2026-09-04 04:23:28 +0000

    deploy: 3667825669e955c41adaadfe1c90d94899ebc22b

---
+++ assets/swimlanesDiagram-VR7AAH4N-CClIEzmZ.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-2Pldy1Bh.js";import{_ as a}from"./mermaid.core-eip5TvmG.js";import"./chunk-5VM5RSS4-1XNBND47.js";import"./chunk-XXDRQBXY-D2GcIcuF.js";import"./chunk-POPQ4Y6H-DFhwjM8L.js";import"./chunk-F27PBJKO-Ci5Lp9-T.js";import"./channel-DTmCf1bg.js";import"./index-CyFWqxib.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
